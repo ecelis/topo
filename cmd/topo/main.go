19deef303215b8dd1f966261b27449974a88bc8e
Ernesto Celis <ernesto@celisdelafuente.net>
2025-02-12 02:34:24 -0600

    initial commit

--- cmd/topo/main.go
+++ cmd/topo/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-    "log"
-    "github.com/ecelis/topo/cmd/topo/service"
+	"log"
+
+	"github.com/ecelis/topo/cmd/topo/service"
 )
 
 func main() {
-        if err := service.Run(); err != nil{
-                log.Fatal(err)
-        }
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ecelis/topo
 
 go 1.23.5
+
+require github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
